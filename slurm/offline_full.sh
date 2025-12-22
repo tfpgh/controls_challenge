d711e3c3a82770834df5f80d0fab8da1af13f1c7
Toby Penner <tobypenner01@gmail.com>
2025-12-21 23:15:34 -0700

    Update slurm

--- slurm/offline_full.sh
+++ slurm/offline_full.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=offline_tune
+#SBATCH --job-name=offline_full
 #SBATCH --output=logs/%A_%a.out
-#SBATCH --partition=gpu-standard
+#SBATCH --partition=gpu-long
 #SBATCH --array=0-7
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16gb
 #SBATCH --gres=gpu:1
-#SBATCH --time=1-00:30:00
+#SBATCH --time=7-00:00:00
 
 echo "Starting Optuna worker ${SLURM_ARRAY_TASK_ID} on $(hostname)"
 echo "SLURM assigned GPU devices: ${CUDA_VISIBLE_DEVICES}"
 echo "GPU device count: $(nvidia-smi --list-gpus | wc -l)"
 
-uv run python -m offline.tune
+uv run -m offline.run --num-workers 8 --worker-id $SLURM_ARRAY_TASK_ID --max-segment 4999 --device cuda
